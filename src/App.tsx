87cbfbbcab887fa1749cb49e4ca7813dbf7e90c3
Vadim T <1hananam1ty@gmail.com>
2024-06-21 21:04:48 +0300

    pew

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,14 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { cn } from '@bem-react/classname';
-
-const cnApp = cn('App');
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className={cnApp()}>
+      <div>
+        div
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
