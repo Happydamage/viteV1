e1740d422b8ca85afaa5de75f3e3db1e41c731c4
Vadim T <1hananam1ty@gmail.com>
2024-06-26 14:23:31 +0300

    router apppage suspense outlet

---
+++ src/app/CodeWarsModule/CodeWarsAddForm.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { cn } from '@bem-react/classname';
+
+const cnCodeWarsAddForm = cn('CodeWarsAddForm');
+
+export const CodeWarsAddForm: FC = () => {
+  return <div className={cnCodeWarsAddForm()}></div>;
+};
